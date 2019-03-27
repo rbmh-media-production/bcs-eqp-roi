2c9f0d43f564e16c0f53bc1360fd6b2d74301583
dmayer1 <daniel.mayer@redbull.com>
2019-03-27 17:18:01 +0100

    Projektdateien hinzufügen.

---
+++ EasyJobRoi/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace EasyJobRoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(794, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnOpenDatavase_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(794, 409);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Equipment Pool ROI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ EasyJobRoi/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Data.SqlClient;
+
+
+namespace EasyJobRoi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpenDatavase_Click(object sender, EventArgs e)
+        {
+
+            string connectionString = "Server = localhost\\SQLEXPRESS; Database = easyjob; Trusted_Connection = True;";
+            string queryString = @"SELECT Item.IdStockItem AS ITEM_ID, 
+                                    Item.SerialNumber AS ITEM_SERIAL,
+                                    Type.Caption AS ITEM_NAME,
+                                    Count(Item2Job.IdStockItem) AS TOTAL_RENTALS,
+                                    Sum(Type2Job.RentalPrice) AS TOTAL_EARNINGS
+                                    
+                                    FROM[easyjob].[dbo].[StockItem] AS Item
+                                    LEFT OUTER JOIN[easyjob].[dbo].[StockType] AS Type
+                                    ON Type.IdStockType = Item.IdStockType
+                                    LEFT OUTER JOIN[easyjob].[dbo].[StockItem2Job] AS Item2Job
+                                    ON Item2Job.IdStockItem = Item.IdStockItem
+                                    LEFT OUTER JOIN[easyjob].[dbo].[StockType2Job] AS Type2Job
+                                    ON Type2Job.IdStockType2Job = Item2Job.IdStockType2Job
+
+                                    GROUP BY Item.IdStockItem,
+                                    Item.SerialNumber,
+                                    Type.Caption
+                                    ORDER BY ITEM_ID";
+
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, cn);
+                cn.Open();
+
+                SqlDataReader dr = command.ExecuteReader(CommandBehavior.CloseConnection);
+                DataTable dtSchema = dr.GetSchemaTable();
+                DataTable dt = new DataTable();
+                // You can also use an ArrayList instead of List<>
+                List<DataColumn> listCols = new List<DataColumn>();
+
+                if (dtSchema != null)
+                {
+                    foreach (DataRow drow in dtSchema.Rows)
+                    {
+                        string columnName = System.Convert.ToString(drow["ColumnName"]);
+                        DataColumn column = new DataColumn(columnName, (Type)(drow["DataType"]));
+                        column.Unique = (bool)drow["IsUnique"];
+                        column.AllowDBNull = (bool)drow["AllowDBNull"];
+                        column.AutoIncrement = (bool)drow["IsAutoIncrement"];
+                        listCols.Add(column);
+                        dt.Columns.Add(column);
+                    }
+                }
+
+                // Read rows from DataReader and populate the DataTable
+                while (dr.Read())
+                {
+                    DataRow dataRow = dt.NewRow();
+                    for (int i = 0; i < listCols.Count; i++)
+                    {
+                        dataRow[((DataColumn)listCols[i])] = dr[i];
+                    }
+                    dt.Rows.Add(dataRow);
+                }
+
+                dataGridView1.DataSource = dt;
+                
+            }
+
+        }
+    }
+}
